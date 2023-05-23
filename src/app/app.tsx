f1964bf5b8d31356aa0cb7f299d75f1216d355a6
juyoung-go <juyounggo@rsqaure.co.kr>
2023-05-23 17:41:56 +0900

    nx project init

---
+++ src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="mint-ui-map-guide-nx" />
+    </StyledApp>
+  );
+}
+
+export default App;
